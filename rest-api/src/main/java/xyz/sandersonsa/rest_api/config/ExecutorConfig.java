14af69738b96a20f1290249943758643f4289880
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2024-12-12 06:53:42 -0300

    refactor 10

--- rest-api/src/main/java/xyz/sandersonsa/rest_api/config/ExecutorConfig.java
+++ rest-api/src/main/java/xyz/sandersonsa/rest_api/config/ExecutorConfig.java
@@ -1,18 +1,18 @@
-package xyz.sandersonsa.rest_api.config;
+// package xyz.sandersonsa.rest_api.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@Configuration
-public class ExecutorConfig {
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(200);
-        executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("custom-executor-");
-        return executor;
-    }
-}
+// @Configuration
+// public class ExecutorConfig {
+//     @Bean
+//     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+//         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//         executor.setCorePoolSize(10);
+//         executor.setMaxPoolSize(200);
+//         executor.setQueueCapacity(100);
+//         executor.setThreadNamePrefix("custom-executor-");
+//         return executor;
+//     }
+// }
